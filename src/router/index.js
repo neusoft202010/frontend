517a0a893f2b0aef9e8f55e534c6ac51c760e705
1170300516 <47969101+1170300516@users.noreply.github.com>
2020-07-30 23:18:05 +0800

    所有模块的index合并

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import HomeMain from "./../components/home/main.vue";
+import HomeMain from "./../components/home/main.vue"
+
 
 import PatientMain from "./../components/patient/main.vue"
 import PatientList from "./../components/patient/list.vue"
@@ -25,9 +26,30 @@ import DoctorMain from "./../components/doctor/main.vue";
 import ClinicMain from "./../components/clinic/main.vue";
 import AdminMain from "./../components/admin/main.vue";
 
+
+import DrugMain from "./../components/drug/main.vue"
+import DrugList from "./../components/drug/list.vue"
+import DrugAdd from "./../components/drug/add.vue"
+import DrugModify from "./../components/drug/modify.vue"
+import DrugView from "./../components/drug/view.vue"
+
+import IndrugMain from "./../components/indrug/main.vue"
+import IndrugList from "./../components/indrug/list.vue"
+import IndrugAdd from "./../components/indrug/add.vue"
+import IndrugModify from "./../components/indrug/modify.vue"
+import IndrugView from "./../components/indrug/view.vue"
+
+import MedicalrecordMain from "./../components/medicalrecord/main.vue"
+import MedicalrecordList from "./../components/medicalrecord/list.vue"
+import MedicalrecordAdd from "./../components/medicalrecord/add.vue"
+import MedicalrecordModify from "./../components/medicalrecord/modify.vue"
+import MedicalrecordView from "./../components/medicalrecord/view.vue"
+
+
 Vue.use(VueRouter)
 
   const routes = [
+
 	{path:"/",name:"homemain",component:HomeMain},
 	{path:"/doctor",name:"doctormain",component:DoctorMain},
 	{path:"/clinic",name:"clinicmain",component:ClinicMain},
@@ -54,12 +76,36 @@ Vue.use(VueRouter)
 		{path:"view/:num", name:"tariffview", component:TariffView},
 		{path:"", redirect:"list"}
 	]}
+
+
+	{path:"/", name:"homemain", component:HomeMain},
+	{path:"/drug", name:"drugmain", component:DrugtMain, children:[
+		{path:"list", name:"druglist", component:DrugList},
+		{path:"add", name:"drugadd", component:DrugAdd},
+		{path:"modify/:num", name:"drugmodify", component:DrugModify},
+		{path:"view/:num", name:"drugview", component:DrugView},
+		{path:"", redirect:"list"}
+	]},
+	{path:"/indrug", name:"indrugmain", component:IndrugMain, children:[
+		{path:"list", name:"indruglist", component:IndrugList},
+		{path:"add", name:"indrugadd", component:IndrugAdd},
+		{path:"modify/:num", name:"indrugmodify", component:IndrugModify},
+		{path:"view/:num", name:"indrugview", component:IndrugView},
+		{path:"", redirect:"list"}
+	]},
+	{path:"/medicalrecord", name:"medicalrecordmain", component:MedicalrecordMain, children:[
+		{path:"list", name:"medicalrecordlist", component:MedicalrecordList},
+		{path:"add", name:"medicalrecordadd", component:MedicalrecordAdd},
+		{path:"modify/:num", name:"medicalrecordmodify", component:MedicalrecordModify},
+		{path:"view/:num", name:"medicalrecordview", component:MedicalrecordView},
+		{path:"", redirect:"list"}
+	]}
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes:routes
+  routes
 })
 
 export default router
